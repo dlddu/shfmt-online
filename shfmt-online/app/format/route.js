fd5fdbc57954bc8507c646e751b20c8f3f748894
dlddu <39251873+dlddu@users.noreply.github.com>
2024-09-23 01:06:16 +0000

    fix: Improve error handling in execCallbackHandler

--- shfmt-online/app/format/route.js
+++ shfmt-online/app/format/route.js
@@ -3,20 +3,26 @@ import { v4 } from "uuid";
 import fs from "node:fs";
 import { NextResponse } from "next/server";
 
-const execAndResolve = (command, resolve) => {
-  const execCallbackHandler = (_err, stdout, _stderr) => resolve(stdout);
-  exec(command, execCallbackHandler);
-};
-const execPromise = (command) =>
-  new Promise((resolve) => execAndResolve(command, resolve));
+const execShfmtDiff = (command) =>
+  new Promise((resolve) =>
+    exec(command, (_err, stdout, _stderr) => resolve(stdout))
+  );
+
+const execShfmt = (command) =>
+  new Promise((resolve) =>
+    exec(command, (err, stdout, stderr) => {
+      if (err !== null) return resolve(stderr);
+      else return resolve(stdout);
+    })
+  );
 
 export async function POST(request) {
   const body = await request.text();
   const requestId = v4();
 
   fs.writeFileSync(requestId, body);
-  const result = await execPromise(`shfmt -s ${requestId}`);
-  const rawDiff = await execPromise(`shfmt -d -s ${requestId}`);
+  const result = await execShfmt(`shfmt -s ${requestId}`);
+  const rawDiff = await execShfmtDiff(`shfmt -d -s ${requestId}`);
   fs.unlinkSync(requestId);
 
   const diff = rawDiff.split("\n").slice(2).join("\n");
